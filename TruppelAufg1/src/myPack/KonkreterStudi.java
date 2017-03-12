2bb69e260d425d74edece39dcec08a3fe92c71d5
StephT1989 <stephan.truppel@students.hs-mainz.de>
2017-03-12 19:07:24 +0100

    Neuerstellung

--- TruppelAufg1/src/myPack/KonkreterStudi.java
+++ TruppelAufg1/src/myPack/KonkreterStudi.java
@@ -1,52 +1,163 @@
+/**
+* Stephan Truppel
+* 12.03.2017, V1.0
+* Dient zur Erstellung eines Studenten mit verschiedenen Eigenschaften, Kommuniziert ausschlieﬂlich mit 
+* einem geeigneten Vermittler.
+* 
+* Open points/ ToDo: keine
+*/
 package myPack;
 
 import java.util.ArrayList;
 
-public class KonkreterStudi extends Studi{
+public class KonkreterStudi extends Kollege {
 
-	
-	
+	private String name;
+	private int fitnessInf;
+	private int fitnessMathe;
+	private Kollege partner;
+
+	/**
+	 * Konstruktor
+	 * 
+	 * @param v
+	 *            Vermittlerobjekt, ueber das kommuniziert werden soll
+	 * @param name
+	 *            Name des Studis
+	 * @param fitnessInf
+	 *            Faehigkeiten Informatik
+	 * @param fitnessMathe
+	 *            Faheigkeiten Mathe
+	 * @param zuordnung
+	 *            z-Objekt, das den Nutzen eines Lernpartners ermitteln kann.
+	 */
 	public KonkreterStudi(Vermittler v, String name, int fitnessInf, int fitnessMathe, Zuord zuordnung) {
 		super(v);
-		
-		// wieso funktioniert hier super und this?
-		super.name = name;
-		super.fitnessInf = fitnessInf;
-		super.fitnessMathe = fitnessMathe;
-		//super.zuordnung = zuordnung;
-		// alle Studis werden initial registriert
+		this.name = name;
+		this.fitnessInf = fitnessInf;
+		this.fitnessMathe = fitnessMathe;
+
 		registrieren();
-		
-		// TODO Auto-generated constructor stub
+
 	}
 
-	public void registrieren(){
+	public void registrieren() {
 		super.registrieren();
 	}
-	
-	public void vermitteln(){
-		super.vermitteln();
+
+	/*
+	 * Studi fordert Lernpartner an
+	 * 
+	 * (non-Javadoc)
+	 * 
+	 * @see myPack.Kollege#zuteilenEinfach()
+	 * 
+	 * 
+	 */
+	public void zuteilenEinfach() {
+		System.out.println(
+				"KonkreterStudi " + this.getName() + " moechte zugeteilt werden und informiert den Vermittler");
+		super.zuteilenEinfach();
 	}
-	
-	public void setzePartner(String partnerName)
-	{
-		super.partner = partnerName;
+
+	/**
+	 * eigene equals-Methode um zwei Studis miteinander vergleichen zu koennen
+	 * 
+	 * @param studi
+	 * @return Objekt identisch, ja/nein
+	 */
+	public boolean equals(KonkreterStudi studi) {
+
+		if (this.name == studi.name && this.fitnessInf == studi.fitnessInf && this.fitnessMathe == studi.fitnessMathe) {
+			return true;
+		}
+		return false;
+
 	}
-	
-	
-	public boolean equals(Studi studi){
-		
-		  if (this.name == studi.name && this.fitnessInf == studi.fitnessInf && this.fitnessMathe == studi.fitnessMathe) {
-		    return true;
-		  }
-		  return false;
-		
+
+	/*
+	 * Ausgabe der Attribute des Studis laut Anforderungen (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return ("[name=" + this.name + ", fitnessInf=" + this.fitnessInf + ", fitnessMathe=" + this.fitnessMathe + "]");
 	}
-	
-	public ArrayList<Long> praefMatrixAusgeben(){
+
+	public ArrayList<Long> praefMatrixAusgeben() {
 		return super.praefMatrixAusgeben();
 	}
-	
-	
-	
+
+	/*
+	 * Wird aufgerufen sobald ein Studi durch eine Zuteilungsanfrage eines
+	 * anderen Studis gepartnert wird. (non-Javadoc)
+	 * 
+	 * @see myPack.Kollege#passiveAenderung(myPack.Kollege)
+	 */
+	public void passiveAenderung(Kollege kol) {
+		kol.partner = this.partner;
+
+	}
+
+	/*
+	 * Wird aufgerufen, wenn ein Studi eine Aenderung angefragt hat.
+	 * Gleichzeitig wird auch der Partner informiert. (non-Javadoc)
+	 * 
+	 * @see myPack.Kollege#aktiveAenderung(myPack.Kollege)
+	 */
+	public void aktiveAenderung(Kollege neuerPartner) {
+		this.partner = neuerPartner;
+		System.out.println("KonkreterStudi " + this.name + " wurde durch Zuteilungsanfrage geaendert. KonkreterStudi "
+				+ this.partner.getName() + " wird durch den Vermittler informiert");
+
+		this.passiveAenderung(this.partner);
+
+	}
+
+	/*
+	 * 
+	 * SETTER - METHODEN
+	 * 
+	 * 
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setFitnessInf(int fintessInf) {
+		this.fitnessInf = fintessInf;
+	}
+
+	public void setFitnessMathe(int fitnessMathe) {
+		this.fitnessMathe = fitnessMathe;
+	}
+
+	public void setPartner(KonkreterStudi partner) {
+		this.partner = partner;
+	}
+
+	/*
+	 * 
+	 * 
+	 * GETTER - METHODEN
+	 * 
+	 */
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getFitnessInf() {
+		return this.fitnessInf;
+	}
+
+	public int getFitnessMathe() {
+		return this.fitnessMathe;
+	}
+
+	public Kollege getPartner() {
+		return this.partner;
+	}
+
 }
--- TruppelAufg1/src/myPack/Testfaelle.java
+++ TruppelAufg1/src/myPack/Testfaelle.java
@@ -1,119 +1,120 @@
+/**
+* Stephan Truppel
+* 12.03.2017, V1.0
+* Enthaelt die Testfaelle laut Anforderung.
+* 
+* Open points/ ToDo: keine
+*/
 package myPack;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random ;
+import java.util.Random;
+
+import org.junit.Test;
 
 public class Testfaelle {
-	
-	public static void main(String[] args) {
+
+	/**
+	 * zu bereits registrierten Studis kommt ein neuer hinzu. Dieser wird zu
+	 * einem geeigneten Partner zugeteilt. Beide Partner werden informiert.
+	 */
+	@Test
+	public void test1() {
+		System.out.println("-- Testfall 1 --");
+		Zuord z = new Zuord();
+
+		KonkreterVermittler konkreterVermittler = new KonkreterVermittler(z);
+		KonkreterStudi expected = new KonkreterStudi(konkreterVermittler, "A", 0, 10, z);
+		KonkreterStudi konkreterStudi2 = new KonkreterStudi(konkreterVermittler, "B", 3, 7, z);
+		KonkreterStudi konkreterStudi3 = new KonkreterStudi(konkreterVermittler, "C", 6, 6, z);
+		System.out.println(
+				"Ausgabe registrierte Studierende: " + konkreterVermittler.getRegistrierteStudierende().toString());
+		KonkreterStudi actual = new KonkreterStudi(konkreterVermittler, "D", 10, 1, z);
+		actual.zuteilenEinfach();
+		assertEquals(expected, actual.getPartner());
+	}
+
+	/**
+	 * Ausgabe einer Praeferenzmatrix pro registriertem Studi und anschließend
+	 * geeignete Partnerzuweisung.
+	 */
+	@Test
+	public void test2() {
+		System.out.println("\n-- Testfall 2 ---");
+		Zuord z = new Zuord();
+		ArrayList<Kollege> arrayliststudiTestfall2 = new ArrayList<Kollege>();
+		KonkreterVermittler konkreterVermittler = new KonkreterVermittler(z);
+
+		KonkreterStudi konkreterStudi1 = new KonkreterStudi(konkreterVermittler, "A", 0, 10, z);
+		KonkreterStudi konkreterStudi2 = new KonkreterStudi(konkreterVermittler, "B", 3, 7, z);
+		KonkreterStudi konkreterStudi3 = new KonkreterStudi(konkreterVermittler, "C", 6, 6, z);
+		KonkreterStudi konkreterStudi4 = new KonkreterStudi(konkreterVermittler, "D", 10, 1, z);
+		int expected = 23;
+
+		System.out.println(
+				"Ausgabe registrierte Studierende: " + konkreterVermittler.getRegistrierteStudierende().toString());
+		System.out.print("Praeferenzmatrix: [");
+		for (Kollege k : konkreterVermittler.getRegistrierteStudierende()) {
+			System.out.print(k.praefMatrixAusgeben().toString());
+		}
+
+		System.out.println("]\n");
+
+		arrayliststudiTestfall2.add(konkreterStudi1);
+		arrayliststudiTestfall2.add(konkreterStudi2);
+		arrayliststudiTestfall2.add(konkreterStudi3);
+		arrayliststudiTestfall2.add(konkreterStudi4);
+
+		konkreterVermittler.zuteilenKomplex(arrayliststudiTestfall2);
+		int actual = konkreterVermittler.getGesamtnutzenGlobal();
+
+		assertEquals(expected, actual);
+	}
+
+	/**
+	 * Ermittlung einer geeigneten Partnerzuweisung für 26 Studenten
+	 * (Näherungslösung). Ermittlung und Ausgabe eines möglichst hohen
+	 * Gesamtnutzens.
+	 * 
+	 * ACHTUNG: Dieser Testfall ist nicht immer reproduzierbar, weil in Klasse
+	 * KonkreterVermittler, Zeile 62 eine zufaellige Reihenfolge der Studenten
+	 * definiert wird. Wenn diese Reihenfolge ungünstig ist, kann der Wert auch
+	 * unter 114 liegen und der Testfall schlaegt fehl. Ich habe meinen
+	 * Näherungsalgorithmus mehere 100.000 Mal ausgeführt, ein Gesamtnutzen von
+	 * 114 wurde niemals überschritten, insofern wird diese Zahl für den
+	 * Testfall verwendet.
+	 */
+	@Test
+	public void test3() {
+		System.out.println("\n-- Testfall 3 --");
 		Zuord z = new Zuord();
 		KonkreterVermittler konkreterVermittler = new KonkreterVermittler(z);
-		
-		
-		ArrayList<Studi> arrayliststudi = new ArrayList<Studi>();
-		Studi[] arrStudis = new Studi[26];
+		ArrayList<Kollege> arrayliststudi = new ArrayList<Kollege>();
+		int expected = 114;
 		Random rand1 = new Random(123);
 		Random rand2 = new Random(123);
-		//rand1 = new Random(123);
-		int  pseudoZufall1;
-		int  pseudoZufall2;
+		int pseudoZufall1;
+		int pseudoZufall2;
 		int i = 0;
 		char alphabet = 'A';
-		while (i <=25 && alphabet<= 'Z'){
-			
+		while (i <= 25 && alphabet <= 'Z') {
+
 			pseudoZufall1 = rand1.nextInt(10);
 			pseudoZufall2 = rand2.nextInt(10);
 			String name = String.valueOf(alphabet);
-			KonkreterStudi konkreterStudi = new KonkreterStudi(konkreterVermittler, name, pseudoZufall1, pseudoZufall2, z);
+			KonkreterStudi konkreterStudi = new KonkreterStudi(konkreterVermittler, name, pseudoZufall1, pseudoZufall2,
+					z);
 			arrayliststudi.add(konkreterStudi);
-			arrStudis[i] = konkreterStudi;
 			i++;
 			alphabet++;
-			
+
 		}
-		//System.out.println(arrStudis.toString());
-		/*
-		KonkreterStudi konkreterStudi = new KonkreterStudi(konkreterVermittler, "A", 0, 10,z);
-		KonkreterStudi konkreterStudi1 = new KonkreterStudi(konkreterVermittler, "B", 3, 7,z);
-		KonkreterStudi konkreterStudi2 = new KonkreterStudi(konkreterVermittler, "C", 6, 6,z);
-		KonkreterStudi konkreterStudi3 = new KonkreterStudi(konkreterVermittler, "D", 10, 1,z);
-		
-		KonkreterStudi konkreterStudi4 = new KonkreterStudi(konkreterVermittler, "E", 0, 10,z);
-		KonkreterStudi konkreterStudi5 = new KonkreterStudi(konkreterVermittler, "F", 3, 7,z);
-		KonkreterStudi konkreterStudi6 = new KonkreterStudi(konkreterVermittler, "g", 6, 6,z);
-		KonkreterStudi konkreterStudi7 = new KonkreterStudi(konkreterVermittler, "h", 10, 1,z);
-		KonkreterStudi konkreterStudi8= new KonkreterStudi(konkreterVermittler, "i", 0, 10,z);
-		KonkreterStudi konkreterStudi9= new KonkreterStudi(konkreterVermittler, "ä", 0, 10,z);
-		KonkreterStudi konkreterStudi10 = new KonkreterStudi(konkreterVermittler, "j", 3, 7,z);
-		KonkreterStudi konkreterStudi11 = new KonkreterStudi(konkreterVermittler, "k", 6, 6,z);
-		KonkreterStudi konkreterStudi12 = new KonkreterStudi(konkreterVermittler, "l", 10, 1,z);
-		KonkreterStudi konkreterStudi13 = new KonkreterStudi(konkreterVermittler, "m", 0, 10,z);
-		KonkreterStudi konkreterStudi14 = new KonkreterStudi(konkreterVermittler, "n", 3, 7,z);
-		KonkreterStudi konkreterStudi15 = new KonkreterStudi(konkreterVermittler, "o", 6, 6,z);
-		KonkreterStudi konkreterStudi16 = new KonkreterStudi(konkreterVermittler, "p", 10, 1,z);
-		KonkreterStudi konkreterStudi17 = new KonkreterStudi(konkreterVermittler, "q", 0, 10,z);
-		KonkreterStudi konkreterStudi18 = new KonkreterStudi(konkreterVermittler, "r", 3, 7,z);
-		KonkreterStudi konkreterStudi19 = new KonkreterStudi(konkreterVermittler, "s", 6, 6,z);
-		KonkreterStudi konkreterStudi20 = new KonkreterStudi(konkreterVermittler, "t", 10, 1,z);
-		KonkreterStudi konkreterStudi21 = new KonkreterStudi(konkreterVermittler, "u", 0, 10,z);
-		KonkreterStudi konkreterStudi22 = new KonkreterStudi(konkreterVermittler, "v", 3, 7,z);
-		KonkreterStudi konkreterStudi23 = new KonkreterStudi(konkreterVermittler, "w", 6, 6,z);
-		KonkreterStudi konkreterStudi24 = new KonkreterStudi(konkreterVermittler, "x", 10, 1,z);
-		KonkreterStudi konkreterStudi25= new KonkreterStudi(konkreterVermittler, "y", 0, 10,z);
-		KonkreterStudi konkreterStudi26 = new KonkreterStudi(konkreterVermittler, "z", 3, 7,z);
-		*/
-		
-		
-		//System.out.println(konkreterVermittler.registrierteStudierende.toString());
-		
-		/*System.out.println("So sieht das dann aus: " + konkreterStudi.praefMatrixAusgeben().toString());
-		System.out.println("So sieht das dann aus: " + konkreterStudi1.praefMatrixAusgeben().toString());
-		System.out.println("So sieht das dann aus: " + konkreterStudi2.praefMatrixAusgeben().toString());
-		System.out.println("So sieht das dann aus: " + konkreterStudi3.praefMatrixAusgeben().toString());
-		*/
-		
-		
-		/*
-		
-		arrayliststudi.add(konkreterStudi);
-		arrayliststudi.add(konkreterStudi1);
-		arrayliststudi.add(konkreterStudi2);
-		arrayliststudi.add(konkreterStudi3);
-		
-		arrayliststudi.add(konkreterStudi4);
-		arrayliststudi.add(konkreterStudi5);
-		arrayliststudi.add(konkreterStudi6);
-		arrayliststudi.add(konkreterStudi7);
-		arrayliststudi.add(konkreterStudi8);
-		arrayliststudi.add(konkreterStudi9);
-		arrayliststudi.add(konkreterStudi10);
-		arrayliststudi.add(konkreterStudi11);
-		arrayliststudi.add(konkreterStudi12);
-		arrayliststudi.add(konkreterStudi13);
-		arrayliststudi.add(konkreterStudi14);
-		arrayliststudi.add(konkreterStudi15);
-		arrayliststudi.add(konkreterStudi16);
-		arrayliststudi.add(konkreterStudi17);
-		arrayliststudi.add(konkreterStudi18);
-		arrayliststudi.add(konkreterStudi19);
-		arrayliststudi.add(konkreterStudi20);
-		arrayliststudi.add(konkreterStudi21);
-		arrayliststudi.add(konkreterStudi22);
-		arrayliststudi.add(konkreterStudi23);
-		arrayliststudi.add(konkreterStudi24);
-		arrayliststudi.add(konkreterStudi25);
-		arrayliststudi.add(konkreterStudi26);
-		*/
-		
-		z.vermittlungBerechnen(arrayliststudi);
-		//z.vermittlungBerechnen(arrStudis);
-		
-		
-		
-		
-		
+
+		konkreterVermittler.zuteilenKomplex(arrayliststudi);
+		int actual = konkreterVermittler.getGesamtnutzenGlobal();
+		assertEquals(expected, actual);
 	}
-	
 
 }
